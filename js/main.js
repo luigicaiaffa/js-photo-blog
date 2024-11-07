320d058a1819ff3b17e721931a0d038f5b15f338
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-11-07 16:18:46 +0100

    add cards stamp

--- js/main.js
+++ js/main.js
@@ -1,31 +1,41 @@
 // # Recupero elementi Html
 
 // card per stampare output
-const cardStamp1 = document.getElementById("card-output-1");
-const cardStamp2 = document.getElementById("card-output-2");
-const cardStamp3 = document.getElementById("card-output-3");
-const cardStamp4 = document.getElementById("card-output-4");
-const cardStamp5 = document.getElementById("card-output-5");
-const cardStamp6 = document.getElementById("card-output-6");
+const cardStamp = document.getElementById("card-output");
+// const cardStamp2 = document.getElementById("card-output-2");
+// const cardStamp3 = document.getElementById("card-output-3");
+// const cardStamp4 = document.getElementById("card-output-4");
+// const cardStamp5 = document.getElementById("card-output-5");
+// const cardStamp6 = document.getElementById("card-output-6");
 
 // card container
 const allCards = document.querySelectorAll(".card");
 
 // # Funzioni
 
-// genero e recupero le immagini e i testi delle card
-fetch("https://jsonplaceholder.typicode.com/photos?_limit=6")
-  .then((response) => response.json())
-  .then((data) => {
-    data.forEach((element) => {
-      imgUrl.push(element.url);
-      imgTitle.push(element.title);
+const generateImageCards = (cardNum) => {
+  fetch(`https://jsonplaceholder.typicode.com/photos?_limit=${cardNum}`)
+    .then((res) => res.json())
+    .then((imgsData) => {
+      imgsData.forEach((img) => {
+        cardStamp.innerHTML += `           
+            <div class="col-12 col-md-6 col-lg-4">
+              <div class="card">
+                <div class="card-body">
+                  <img src="${img.url}" alt="img" />
+                  <div class="py-3">${img.title}</div>
+                </div>
+              </div>
+            </div>`;
+      });
+
+      // debug
+      console.log(imgsData);
     });
-    console.log(imgUrl);
-    console.log(imgTitle);
-  });
+};
 
 // # Svolgimento
+const cardGenNum = 6;
 
-let imgUrl = [];
-let imgTitle = [];
+// # Output
+generateImageCards(cardGenNum);
