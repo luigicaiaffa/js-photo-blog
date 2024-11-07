5c4a9e2b62c8cb204f2fb3699d182bc658019589
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-11-07 15:25:23 +0100

    recupero el html

--- js/main.js
+++ js/main.js
@@ -1,12 +1,31 @@
-// # Fetch
+// # Recupero elementi Html
+
+// card per stampare output
+const cardStamp1 = document.getElementById("card-output-1");
+const cardStamp2 = document.getElementById("card-output-2");
+const cardStamp3 = document.getElementById("card-output-3");
+const cardStamp4 = document.getElementById("card-output-4");
+const cardStamp5 = document.getElementById("card-output-5");
+const cardStamp6 = document.getElementById("card-output-6");
+
+// card container
+const allCards = document.querySelectorAll(".card");
+
+// # Funzioni
 
 // genero e recupero le immagini e i testi delle card
 fetch("https://jsonplaceholder.typicode.com/photos?_limit=6")
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
-    data.forEach((el) => {
-      console.log(el.url);
-      console.log(el.title);
+    data.forEach((element) => {
+      imgUrl.push(element.url);
+      imgTitle.push(element.title);
     });
+    console.log(imgUrl);
+    console.log(imgTitle);
   });
+
+// # Svolgimento
+
+let imgUrl = [];
+let imgTitle = [];
